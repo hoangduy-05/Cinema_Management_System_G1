48aa8b5379de87fb3550ace2f3d34e76b1a12476
duongvn9 <ngocduongvu9999@gmail.com>
2026-07-17 21:27:24 +0700

    fix header

--- frontend/src/layouts/CgvHeader.jsx
+++ frontend/src/layouts/CgvHeader.jsx
@@ -11,27 +11,31 @@ export default function CgvHeader() {
   return (
     <>
       <div className="cgv-topbar">
-        <span>🎟 TIN MỚI &amp; ƯU ĐÃI</span>
-        <Link to={PATHS.MY_TICKETS}>🎫 VÉ CỦA TÔI</Link>
-        {user ? (
-          <span>
-            👤 XIN CHÀO, <b>{user.username}</b>!{" "}
-            <a onClick={() => { dispatch(logout()); navigate("/"); }} style={{ cursor: "pointer" }}>THOÁT</a>
-          </span>
-        ) : (
-          <Link to="/login">👤 ĐĂNG NHẬP/ ĐĂNG KÝ</Link>
-        )}
-        <span className="lang"><span className="on">VN</span><span>EN</span></span>
+        <div className="cgv-topbar-content">
+          <span>🎟 TIN MỚI &amp; ƯU ĐÃI</span>
+          <Link to={PATHS.MY_TICKETS}>🎫 VÉ CỦA TÔI</Link>
+          {user ? (
+            <span>
+              👤 XIN CHÀO, <b>{user.username}</b>!{" "}
+              <a onClick={() => { dispatch(logout()); navigate("/"); }} style={{ cursor: "pointer" }}>THOÁT</a>
+            </span>
+          ) : (
+            <Link to="/login">👤 ĐĂNG NHẬP/ ĐĂNG KÝ</Link>
+          )}
+          <span className="lang"><span className="on">VN</span><span>EN</span></span>
+        </div>
       </div>
 
       <div className="cgv-stripe" />
       <div className="cgv-nav">
-        <Link to="/" className="logo">F-Cinema</Link>
-        <div className="menu">
-          <Link to="/">PHIM</Link>
-          <a>RẠP F-Cinema</a>
+        <div className="cgv-nav-content">
+          <Link to="/" className="logo">F-Cinema</Link>
+          <div className="menu">
+            <Link to="/">PHIM</Link>
+            <a>RẠP F-Cinema</a>
+          </div>
+          <Link to="/" className="buy">MUA VÉ NGAY</Link>
         </div>
-        <Link to="/" className="buy">MUA VÉ NGAY</Link>
       </div>
       <div className="cgv-stripe" />
     </>
